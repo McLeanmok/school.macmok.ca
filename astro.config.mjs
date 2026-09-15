799e2739c63a701de337ce947da4eeb5166203c2
McLeanmok <165439265+McLeanmok@users.noreply.github.com>
2026-09-15 13:25:19 -0700

    Added drawing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import purgecss from 'astro-purgecss';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
-    // Add purgecss support to Astro. This will not output anything that is not used in your final build.
-  integrations: [purgecss({
-    fontFace: true,
-   variables: true,
-  }),],
+  adapter: cloudflare(),
+  vite: {
+    build: {
+      cssMinify: false,
+    },
+  },
 });
---
+++ public/labs/processing/first-sketch/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(1920, 1080);
+}
+
+function draw() {
+// canvas background()
+  background(255);
+  // square
+fill(35, 201, 255);
+  stroke(170, 170, 40);
+  strokeWeight(8);
+rect(80, 80, 220, 220);
+  
+  fill(35, 201, 200);
+  noStroke();
+  
+rect(180, 180, 240, 240, 208);
+
+ // line 
+  stroke(0,0,0,0)
+  line(20, 20, 20, 20)
+}
